2ea6b992068a8cd15449a959d68c16f526e8ebd0
Noé Falzon <1331434+Castux@users.noreply.github.com>
2022-03-15 10:05:13 +0200

    Removed escape key to quit the viewer
    Since the terminal is in raw mode, the escape key is used to start escape sequences, and does not register as a single key press.

--- tools/viewer.lua
+++ tools/viewer.lua
@@ -164,7 +164,7 @@ function print_help()
     printnl("[←/→: previous/next turn, ↑/↓, previous/next round]")
     printnl("[A/D: start/end]")
     printnl("[space: play next ten rounds, enter: play whole match]")
-    printnl("[esc/Q: quit]")
+    printnl("[Q: quit]")
 end
 
 function clamp(x, min, max)
@@ -235,7 +235,7 @@ function input_loop(turns, pname0, pname1)
                 play(10 * 10)
             elseif k == 13 or k == 10 then
                 play(#turns)
-            elseif k == term.keys.esc or k == key 'q' then
+            elseif k == key 'q' then
                 break
             end
 
